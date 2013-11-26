d145995e941596bb682b90c22e4cc00885898ed5
Anoop Thomas Mathew <atm@profoundis.com>
2013-11-26 09:31:29 +0530

    min.gz files to be updated through compress.sh

--- compress.sh
+++ compress.sh
@@ -4,3 +4,6 @@ yuicompressor auto.micro.js -o auto.micro.min.js
 tar -cvf auto.max.js.gz auto.max.js
 tar -cvf auto.standard.js.gz auto.standard.js
 tar -cvf auto.micro.js.gz auto.micro.js
+tar -cvf auto.max.min.js.gz auto.max.min.js
+tar -cvf auto.standard.min.js.gz auto.standard.min.js
+tar -cvf auto.micro.min.js.gz auto.micro.min.js
